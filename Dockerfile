28ac33baabbdc22738b5886989d6fbbb6592ff1f
michal <michal.hecka@gmail.com>
2024-07-28 21:28:50 +0200

    edited comments to english, removed minor, major versioning of python in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM node:18-alpine
 WORKDIR /app
 COPY . .
 RUN yarn add sqlite3
-RUN apk add php82
-RUN apk add php82-sqlite3
-RUN apk add php82-curl
+RUN apk add php
+RUN apk add php-sqlite3
+RUN apk add php-curl
 RUN mkdir /etc/db
 RUN php database_setup.php
 RUN chmod 755 /app/setup.sh
--- tinygs.php
+++ tinygs.php
@@ -5,7 +5,7 @@ ini_set("display_errors", 1);
 //set_time_limit(15);
 
 
-//używamy curl'a do pobrania danych z api
+//curl used to fetch data from API
 $ch = curl_init();
 $sat = $satelliteName;
 curl_setopt($ch, CURLOPT_URL, 'https://api.tinygs.com/v2/packets?satellite=' . $sat);
@@ -16,19 +16,19 @@ $headers = array(
 );
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 
-curl_setopt($ch, CURLOPT_HEADER, 0); // return headers 0 no 1 yes
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); // return page 1:yes
-curl_setopt($ch, CURLOPT_TIMEOUT, 20); // http request timeout 20 seconds
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); // Follow redirects, need this if the url changes
-curl_setopt($ch, CURLOPT_MAXREDIRS, 2); //if http server gives redirection responce
+curl_setopt($ch, CURLOPT_HEADER, 0); 			// return headers 0 no 1 yes
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 	// return page 1:yes
+curl_setopt($ch, CURLOPT_TIMEOUT, 20); 			// http request timeout 20 seconds
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false); 	// Follow redirects, need this if the url changes
+curl_setopt($ch, CURLOPT_MAXREDIRS, 2); 		//if http server gives redirection responce
 curl_setopt($ch, CURLOPT_USERAGENT,"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/115.0");
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // false for https
-$data = curl_exec($ch); // execute the http request
-curl_close($ch); // close the connection
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); 	// false for https
+$data = curl_exec($ch); 				// execute the http request
+curl_close($ch); 					// close the connection
 
 
 /*
-Struktura tablicy:
+Database table structure:
 CREATE TABLE tb_satelite (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 name STRING,
